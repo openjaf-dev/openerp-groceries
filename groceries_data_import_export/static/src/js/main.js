6271ba4ff0053a1be159641b104e2df2ab4ce298
José Andrés Hernández Bustio <jbustio@gmail.com>
2014-10-24 14:11:59 -0400

    add groceries_data_import_export

---
+++ groceries_data_import_export/static/src/js/main.js
@@ -0,0 +1,12 @@
+
+openerp.groceries_data_import_export = function(instance) {
+
+    instance.groceries_data_import_export = {};
+
+    var module = instance.groceries_data_import_export;
+
+    openerp_view_form_ext(instance,module);
+
+};
+
+    
